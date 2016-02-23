d851521fcafe356403704ff06311c7421d63d700
Vlasenko Mariya W <xmashka@gmail.com>
2016-02-23 16:59:00 +0200

    add one more output, change gitignore (out)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,9 @@
  * Created by user on 20.02.2016.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
 }
